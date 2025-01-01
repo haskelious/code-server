0d53dfa5eb8c7435dca5812d6866b6f37acb5d49
Stephane <stephane@frozenkiwi.net>
2025-01-01 20:18:52 +0100

    add alternative linkers

--- envs/import_rust.sh
+++ envs/import_rust.sh
@@ -1,11 +1,20 @@
 #!/bin/env bash
 
 # install recommended packages into the environment
-nix-env -f '<nixpkgs>' --log-format bar -iA clang cmake gnumake rustup
+nix-env -f channel:nixpkgs-unstable --log-format bar -iA clang lld cmake gnumake rustup
+
+# install altenative linker
+nix-env -f channel:nixos-24.11 --log-format bar -iA mold
+mkdir -p ~/.cargo && cat > ~/.cargo/cargo.toml << EOF
+[target.x86_64-unknown-linux-gnu]
+linker = "clang"
+rustflags = ["-C", "link-arg=-fuse-ld=/path/to/mold"]
+EOF
 
 # use rustup to install the toolchain
 rustup toolchain install stable
 rustup component add rust-analyzer
+cargo install cargo-expand
 
 # install recommended VSCode extensions
 code-server --install-extension rust-lang.rust-analyzer
